11f78ac1f1696e9549c18ca179cc81f6f45f2d73
tluciad <tluciad@gmail.com>
2022-07-26 15:10:03 -0500

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_height:
+ *
+ * @param tree
+ * @return size_t
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t left = 0;
+    size_t right = 0;
+
+    if (!tree || !tree->parent)
+        return (0);
+
+    if (!tree->parent && !tree->parent)
+        return (0);
+    else
+    {
+        left = binary_tree_depth(tree->parent);
+        right = binary_tree_depth(tree->parent);
+        if (left > right)
+            return (right + 1);
+        else
+            return (left + 1);
+    }
+}
